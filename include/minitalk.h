ca926f195cf4ce6668d440e069cbf9e650639d32
Sawada-Mai <kg.pinono.571016@gmail.com>
2024-11-02 18:00:32 +0900

    first commit

---
+++ include/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msawada <msawada@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 17:59:12 by msawada           #+#    #+#             */
+/*   Updated: 2024/11/02 18:00:00 by msawada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+
+#endif
